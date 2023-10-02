d0c4d38ac8caf1ac51d00c47e944f11e017a3162
sam.lear <samuelcharleslear@gmail.com>
2023-10-02 19:41:36 +0100

    Removed DrugRoomSide concept from the Drug Room Door. The door now blocks access in both directions.

--- DispensoryMod3/DrugRoomDoor.cs
+++ DispensoryMod3/DrugRoomDoor.cs
@@ -16,8 +16,6 @@ namespace DispensaryMod
         private static Texture2D ToggleIcon = ContentFinder<Texture2D>.Get("UI/Commands/HideZone");
         private static Texture2D ToggleOpen = ContentFinder<Texture2D>.Get("UI/Commands/Halt");
 
-        private RoomSide drugRoomSide = RoomSide.NotSet;
-
         private CompPowerTrader cPower;
 
         private DrugDoorSettings drugDoorSettings;
@@ -43,40 +41,9 @@ namespace DispensaryMod
             return drugDoorSettings;
         }
 
-        private Room getDrugRoom()
-        {
-            if (drugRoomSide == RoomSide.NotSet) SetRoomSide();
-            if (drugRoomSide == RoomSide.NorthSide) return getIndoorRoom(getNCell());
-            else if (drugRoomSide == RoomSide.SouthSide) return getIndoorRoom(getSCell());
-            else return null;
-        }
-
-        private IntVec3 getNCell() { return (base.Position + IntVec3.North.RotatedBy(base.Rotation)); }
-        private IntVec3 getSCell() { return (base.Position + IntVec3.South.RotatedBy(base.Rotation)); }
-
-        private void SetRoomSide()
-        {
-            IntVec3 NCell = getNCell();
-            IntVec3 SCell = getSCell();
-            if (!NCell.UsesOutdoorTemperature(this.Map)) drugRoomSide = RoomSide.SouthSide;
-            if (!SCell.UsesOutdoorTemperature(this.Map)) drugRoomSide = RoomSide.NorthSide;
-            if (!NCell.UsesOutdoorTemperature(this.Map) & !SCell.UsesOutdoorTemperature(this.Map))
-            {
-                drugRoomSide = RoomSide.SouthSide;
-            }
-        }
-
-        private void ToggleActiveRoom()
-        {
-            if (drugRoomSide == RoomSide.NorthSide) drugRoomSide = RoomSide.SouthSide;
-            else drugRoomSide = RoomSide.NorthSide;
-        }
-
         public override void ExposeData()
         {
             base.ExposeData();
-            //Scribe_Values.Look<Room>(ref DrugRoom, "DrugRoom", null);
-            Scribe_Values.Look<RoomSide>(ref drugRoomSide, "RoomToggle", RoomSide.NotSet);
             //Scribe_Values.Look<bool>(ref ToggleSetOpen, "ToggleSetOpen", true);
         }
 
@@ -94,28 +61,15 @@ namespace DispensaryMod
 
         private bool allowOpen(Pawn p)
         {
-            Room drugRoom = getDrugRoom();
-
             if (!this.powerComp.PowerOn) return true;
-
-            if (drugRoom != null)
-            {
-                if (p.GetRoom() == drugRoom) return true;
-                if (p.IsColonist)
-                {
-                    return drugDoorSettings.PolicyAllowed(p.drugs.CurrentPolicy);
-                }
-                return false;
-            }
-            return true;
+            if (!p.IsColonist) return false;
+            
+            return drugDoorSettings.PolicyAllowed(p.drugs.CurrentPolicy);
         }
 
         public override void DrawExtraSelectionOverlays()
         {
-            Room drugRoom = getDrugRoom();
 
-            base.DrawExtraSelectionOverlays();
-            if (drugRoom != null) GenDraw.DrawFieldEdges(drugRoom.Cells.ToList(), Color.green);
         }
 
         private void onOpenDoor()
@@ -142,13 +96,6 @@ namespace DispensaryMod
         {
             List<Gizmo> list = new List<Gizmo>();
 
-            Command_Action ca1 = new Command_Action();
-            ca1.icon = ToggleIcon;
-            ca1.activateSound = SoundDef.Named("Click");
-            ca1.defaultLabel = "Toggle DrugRoom Side";
-            ca1.action = new Action(this.ToggleActiveRoom);
-            list.Add(ca1);
-
             Command_Action ca2 = new Command_Action();
             ca2.icon = ToggleOpen;
             ca2.activateSound = SoundDef.Named("click");
@@ -159,13 +106,6 @@ namespace DispensaryMod
 
             IEnumerable<Gizmo> commands = base.GetGizmos();
             return (commands == null) ? list.AsEnumerable<Gizmo>() : list.AsEnumerable<Gizmo>().Concat(commands);
-        }
-
-        private enum RoomSide
-        {
-            NotSet = 0,
-            NorthSide=1,
-            SouthSide=2
         }
 
     }
